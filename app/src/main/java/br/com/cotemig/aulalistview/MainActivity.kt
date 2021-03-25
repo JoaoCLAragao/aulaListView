2bb6158fe86598244fb5cc9a1ad0b6b5410a0d84
JoaoCLAragao <71600841@aluno.faculdadecotemig.br>
2021-03-25 20:35:58 -0300

    Termino da aula
    Fim do app criado na aula.

--- app/src/main/java/br/com/cotemig/aulalistview/MainActivity.kt
+++ app/src/main/java/br/com/cotemig/aulalistview/MainActivity.kt
@@ -10,12 +10,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var m = ArrayList<String>()
-            m.add("Oi, como você ta?")
+        m.add("Oi, como você ta?")
         m.add("Eu to bem e você?")
         m.add("To bem também")
         m.add("Ta fazendo a aula de mobile do dirceu? Ele acha que o méxico fica na europa. muito doidão da cabeça")
         m.add("Me IGNORA NÃO")
         m.add("Você foi bloqueado")
+
 
         var listView = findViewById<ListView>(R.id.listview)
         listView.adapter = MessageAdapter(this, m)
